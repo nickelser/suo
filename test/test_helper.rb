754d7d8faf2ea1fc862ce97fb53902d36453e287
Nick Elser <nick.elser@gmail.com>
2015-04-13 10:12:26 -0700

    report test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
+
 require "suo"
 require "thread"
 require "minitest/autorun"
